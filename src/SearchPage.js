2071e18da9a77a4eb1eeb2e319337a438d0a2703
Jerud-Moyer <jerudmoyer@gmail.com>
2020-08-26 09:01:36 -0700

    hamburger-onion

---
+++ src/SearchPage.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+
+import { cuisines, diets } from './data.js';
+import { searchRecipes } from './whats_cookn_api.js';
+
+
+export default class searchPage extends Component {
+    state = {
+        searchByCuisine: '',
+        searchByDiet: '',
+        searchByIngredients: '',
+        isLoading: false,
+        
+        currentPage: 1,
+        totalPages: 1,
+    }
+
+    componentDidMount = async () => {
+        
+        
+    }
+
+    makeRequest = async () => {
+        this.setState({ isLoading: true });
+        const options = {
+        cuisine: this.state.searchByCuisine,
+        diet: this.state.searchByDiet,
+        ingredients: this.state.searchByIngredients
+        }
+        console.log(options);
+        const recipeData = await searchRecipes(options);
+        await this.setState({
+            results: recipeData.body.results,
+            totalPages: Math.ceil(recipeData.body.count / 20),
+            isLoading: false,
+            
+        })
+        console.log(recipeData);
+
+        
+    }
+
+        handlePrevClick = async () => {
+            await this.setState({ currentPage: Number(this.state.currentPage) -1 })
+            await this.makeRequest()
+      }
+    
+        handleNextClick = async () => {
+            await this.setState({ currentPage: Number(this.state.currentPage) +1 })
+            await this.makeRequest()
+      }
+      handleFormSubmit = async (e) => {
+            e.preventDefault();
+            await this.makeRequest()
+
+      } 
+      handleItemChange = async (e) => {
+          const target = e.target;
+          const name = target.name;
+          const val = target.value;
+          await this.setState({[name]:val});
+      }
+
+    
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleFormSubmit}>
+                <select name='searchByCuisine' value={this.state.searchByCuisine}onChange={this.handleItemChange}>
+                        {
+                        cuisines.map(cuisine => <option value={cuisine}>{cuisine}</option>)
+                        }
+                </select>
+                <select  name='searchByDiet' value={this.state.searchByDiet}onChange={this.handleItemChange}>
+                        {
+                        diets.map(diet => <option value={diet}>{diet}</option>)
+                        }
+                </select>
+                <input name='searchByIngredients' value={this.state.searchByIngredients} onChange={this.handleItemChange}/>
+                <button>Search</button>
+                </form>
+
+                
+            </div>
+        )
+    }
+}
